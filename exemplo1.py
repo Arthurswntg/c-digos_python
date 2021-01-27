231aabde02fdc971b6fd5ff7ebd54d7c3d5a1dea
Arthur Sampaio Pereira <arthursampaiop@outlook.com>
2021-01-27 14:49:45 -0300

    exemplo 1

---
+++ exemplo1.py
@@ -0,0 +1,4 @@
+nome = input("Qual o seu nome? ")
+end = input("\nQual o seu endereço? ")
+idade = int(input("\nQual a sua idade? "))
+print("\n{} mora no endereço {} e tem {} anos\n\n".format(nome, end, idade))
